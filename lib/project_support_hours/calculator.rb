4c0e0fa9676e189756e8736f0a094a2707a4d851
Eric Davis <edavis@littlestreamsoftware.com>
2009-09-10 14:17:38 -0700

    [#3014] Added API to get the end date for a project.

--- lib/project_support_hours/calculator.rb
+++ lib/project_support_hours/calculator.rb
@@ -5,5 +5,11 @@ module ProjectSupportHours
       start_date_field = project.custom_value_for(ProjectSupportHours::Mapper.start_date)
       start_date_field ? start_date_field.value : nil
     end
+
+    # Gets the end_date for the project if set
+    def self.end_date_for(project)
+      end_date_field = project.custom_value_for(ProjectSupportHours::Mapper.end_date)
+      end_date_field ? end_date_field.value : nil
+    end
   end
 end
--- test/unit/lib/project_support_hours/calculator_test.rb
+++ test/unit/lib/project_support_hours/calculator_test.rb
@@ -2,6 +2,7 @@ require File.dirname(__FILE__) + '/../../../test_helper'
 
 class ProjectSupportHours::CalculatorTest < Test::Unit::TestCase
   context '#start_date_for' do
+
     context 'configured project' do
       should 'return the start date of the project' do
         @project = Project.generate!
@@ -14,11 +15,37 @@ class ProjectSupportHours::CalculatorTest < Test::Unit::TestCase
     end
 
     context 'not configured project' do
+
       should 'return nil' do
         @project = Project.generate!
         assert_nil ProjectSupportHours::Calculator.start_date_for(@project)
       end
+
     end
+
   end
+
+  context '#end_date_for' do
+
+    context 'configured project' do
+      should 'return the end date of the project' do
+        @project = Project.generate!
+        @end_date_custom_field = ProjectCustomField.generate!(:field_format => 'date')
+        @project.custom_values << CustomValue.spawn(:custom_field => @end_date_custom_field, :customized_id => @project, :value => '2009-09-22')
+        configure_plugin('end_date_field' => @end_date_custom_field.id.to_s)
+
+        assert_equal '2009-09-22', ProjectSupportHours::Calculator.end_date_for(@project)
+      end
+    end
+
+    context 'not configured project' do
+      should 'return nil' do
+        @project = Project.generate!
+        assert_nil ProjectSupportHours::Calculator.end_date_for(@project)
+      end
+    end
+
+  end
+
 end
 
