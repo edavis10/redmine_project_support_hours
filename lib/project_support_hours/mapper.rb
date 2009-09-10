9d2d563628d80f6b9cf92677ad8b45d22d66ba22
Eric Davis <edavis@littlestreamsoftware.com>
2009-09-10 13:52:11 -0700

    [#3014] Implemented the rest of ProjectSupportHours::Mapper to get the config.

--- lib/project_support_hours/mapper.rb
+++ lib/project_support_hours/mapper.rb
@@ -6,5 +6,19 @@ module ProjectSupportHours
         return ProjectCustomField.find_by_field_format_and_id('float', configuration['hours_field'].to_i)
       end
     end
+
+    def self.start_date
+      configuration = Setting.plugin_redmine_project_support_hours
+      if configuration['start_date_field']
+        return ProjectCustomField.find_by_field_format_and_id('date', configuration['start_date_field'].to_i)
+      end
+    end
+
+    def self.end_date
+      configuration = Setting.plugin_redmine_project_support_hours
+      if configuration['end_date_field']
+        return ProjectCustomField.find_by_field_format_and_id('date', configuration['end_date_field'].to_i)
+      end
+    end
   end
 end
--- test/unit/lib/project_support_hours/mapper_test.rb
+++ test/unit/lib/project_support_hours/mapper_test.rb
@@ -2,5 +2,7 @@ require File.dirname(__FILE__) + '/../../../test_helper'
 
 class ProjectSupportHours::MapperTest < Test::Unit::TestCase
   should_get_the_project_custom_field_for :hours_field, :using => :hours, :format => 'float'
+  should_get_the_project_custom_field_for :start_date_field, :using => :start_date, :format => 'date'
+  should_get_the_project_custom_field_for :end_date_field, :using => :end_date, :format => 'date'
 end
 
