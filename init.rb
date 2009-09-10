9bdc7003b9f78adb5f5a64d51c2e7fc32b0048d8
Eric Davis <edavis@littlestreamsoftware.com>
2009-09-10 09:00:13 -0700

    Generated plugin

---
+++ init.rb
@@ -0,0 +1,8 @@
+require 'redmine'
+
+Redmine::Plugin.register :redmine_project_support_hours do
+  name 'Redmine Project Support Hours plugin'
+  author 'Author name'
+  description 'This is a plugin for Redmine'
+  version '0.0.1'
+end
