b434005f3d52e70b862435ded11b15d7071f2849
Eric Davis <edavis@littlestreamsoftware.com>
2009-09-15 11:19:35 -0700

    [#3014] Reformatted the Support Time sidebar
    * Round hours to two decimal places
    * Change the layout to be in a list

--- lib/project_support_hours/hooks/project_hooks.rb
+++ lib/project_support_hours/hooks/project_hooks.rb
@@ -5,17 +5,15 @@ module ProjectSupportHours
       def view_projects_show_sidebar_bottom(context={})
         start_date = ProjectSupportHours::Calculator.start_date_for(context[:project])
         end_date = ProjectSupportHours::Calculator.end_date_for(context[:project])
-        hours_label = l(:project_support_hours_text_hour_report, {
-                          :remaining => ProjectSupportHours::Calculator.total_hours_remaining_for(context[:project]),
-                          :used => ProjectSupportHours::Calculator.total_hours_used_for(context[:project]),
-                          :total => ProjectSupportHours::Calculator.total_support_hours_for(context[:project])
-                        })
+        total_hours = ProjectSupportHours::Calculator.total_support_hours_for(context[:project]).to_f.round_with_precision(2)
+        remaining_hours = ProjectSupportHours::Calculator.total_hours_remaining_for(context[:project]).to_f.round_with_precision(2)
 
         return context[:controller].send(:render_to_string, {
                                            :partial => 'project_support_hours/sidebar',
                                            :locals => {
                                              :project => context[:project],
-                                             :hours_label => hours_label,
+                                             :total_hours => total_hours,
+                                             :remaining_hours => remaining_hours,
                                              :start_date => start_date,
                                              :end_date => end_date
                                            },
--- test/integration/project_overview_hooks_test.rb
+++ test/integration/project_overview_hooks_test.rb
@@ -16,7 +16,7 @@ class ProjectOverviewHooksTest < ActionController::IntegrationTest
         should "not be displyed if it's empty" do
           get_project_overview
 
-          assert_select '#project-support-hours p.start-date', 0
+          assert_select '#project-support-hours li.start-date', 0
         end
 
         should "be formatted based on the user's language" do
@@ -24,7 +24,7 @@ class ProjectOverviewHooksTest < ActionController::IntegrationTest
 
           get_project_overview
 
-          assert_select '#project-support-hours p.start-date', "Started on 09/10/2009"
+          assert_select '#project-support-hours li.start-date', "Start Date: 09/10/2009"
         end
       end
       
@@ -32,7 +32,7 @@ class ProjectOverviewHooksTest < ActionController::IntegrationTest
         should "not be displyed if it's empty" do
           get_project_overview
 
-          assert_select '#project-support-hours p.start-date', 0
+          assert_select '#project-support-hours li.start-date', 0
         end
 
         should "be formatted  based on the user's language" do
@@ -40,7 +40,7 @@ class ProjectOverviewHooksTest < ActionController::IntegrationTest
 
           get_project_overview
 
-          assert_select '#project-support-hours p.end-date', "Ending on 09/22/2009"
+          assert_select '#project-support-hours li.end-date', "End Date: 09/22/2009"
         end
       end
 
@@ -49,23 +49,7 @@ class ProjectOverviewHooksTest < ActionController::IntegrationTest
 
         get_project_overview
 
-        assert_select "#project-support-hours p.support-hours", /100\.25/
-      end
-
-      should "show the hours used" do
-        user = User.generate_with_protected!
-        activity = TimeEntryActivity.generate!
-      
-        assert_difference 'TimeEntry.count', 3 do
-          3.times do 
-            TimeEntry.generate!(:user => user, :activity => activity, :project => @project, :hours => 10.0)
-          end
-        end
-
-        get_project_overview
-
-        assert_select "#project-support-hours p.support-hours", /30\.0/
-
+        assert_select "#project-support-hours li.support-hours-total", /100\.25/
       end
 
       should "show the hours remaining" do
@@ -82,7 +66,7 @@ class ProjectOverviewHooksTest < ActionController::IntegrationTest
 
         get_project_overview
 
-        assert_select "#project-support-hours p.support-hours", /70\.25/
+        assert_select "#project-support-hours li.support-hours-remaining", /70\.25/
 
       end
     end
