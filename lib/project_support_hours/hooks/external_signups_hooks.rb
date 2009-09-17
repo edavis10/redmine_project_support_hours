c5b888ea53fe1ea89066e819078592f6c9c358a0
Eric Davis <edavis@littlestreamsoftware.com>
2009-09-17 14:16:07 -0700

    [#3016] Hooking into the external signups plugin.

---
+++ lib/project_support_hours/hooks/external_signups_hooks.rb
@@ -0,0 +1,11 @@
+module ProjectSupportHours
+  module Hooks
+    class ExternalSignupsHooks < Redmine::Hook::ViewListener
+
+      def plugin_external_signup_controller_external_signups_create_pre_validate(context={})
+      end
+
+    end
+  end
+end
+
---
+++ test/unit/lib/project_support_hours/hooks/external_signups_hooks_test.rb
@@ -0,0 +1,55 @@
+require File.dirname(__FILE__) + '/../../../../test_helper'
+
+class ProjectSupportHours::Hooks::ExternalSignupsHooks < Test::Unit::TestCase
+  include Redmine::Hook::Helper
+
+  def controller
+    @controller ||= ExternalSignupsController.new
+    @controller.response ||= ActionController::TestResponse.new
+    @controller
+  end
+
+  def request
+    @request ||= ActionController::TestRequest.new
+  end
+
+  def hook(args={})
+    call_hook :plugin_external_signup_controller_external_signups_create_pre_validate, args
+  end
+    
+  context "#plugin_external_signup_controller_external_signups_create_pre_validate" do
+    setup do
+      setup_plugin_configuration
+      @project = Project.generate!
+      @user = User.generate_with_protected!
+    end
+
+    # These "should" be automatic but lets test our assumptions
+    should "have the custom fields added to the project" do
+      hook :project => @project, :user => @user
+      assert !@project.custom_values.empty?
+      assert_contains @project.custom_values.collect(&:custom_field), @hours_custom_field
+      assert_contains @project.custom_values.collect(&:custom_field), @start_date_custom_field
+      assert_contains @project.custom_values.collect(&:custom_field), @end_date_custom_field
+    end
+    
+    context "for hours" do
+      should "do nothing if the hours_field is not configured"
+      should "do nothing if the form paramters don't have hours"
+      should "save the form parameters to the project"
+    end
+    
+    context "for start date" do
+      should "do nothing if the start_date_field is not configured"
+      should "do nothing if the form paramters don't have a start date"
+      should "save the form parameters to the project"
+    end
+
+    context "for end date" do
+      should "do nothing if the end_date_field is not configured"
+      should "do nothing if the form paramters don't have a end date"
+      should "save the form parameters to the project"
+    end
+
+  end
+end
